1c89a9ed5427e7140aba77b67a8b23bd99137b31
Alex <alexkniesz@gmx.de>
2022-02-25 19:11:17 +0100

    added last files

---
+++ UI/HealthUI.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var hearts = 4 setget set_hearts
+var max_hearts = 4 setget set_max_hearts
+
+onready var heartUIFull = $HeartUIFull
+onready var heartUIEmpty = $HeartUIEmpty
+
+func set_hearts(value):
+	hearts = clamp(value, 0 ,max_hearts)
+	if heartUIFull != null:
+		heartUIFull.rect_size.x = 15 * hearts
+	
+func set_max_hearts(value):
+	max_hearts = max(value, 1)
+	self.hearts = min(hearts, max_hearts)
+	if heartUIEmpty != null:
+		heartUIEmpty.rect_size.x = 15 * max_hearts
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.max_hearts = PlayerStats.max_health
+	self.hearts = PlayerStats.health
+# warning-ignore:return_value_discarded
+	PlayerStats.connect("health_changed", self, "set_hearts")
+# warning-ignore:return_value_discarded
+	PlayerStats.connect("max_health_changed", self, "set_max_hearts")
